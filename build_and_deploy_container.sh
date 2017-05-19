42e3ff56171c057c03b30b68858c5298f52e25c7
Peter Boothe <pboothe@pboothe2.nyc.corp.google.com>
2017-05-19 15:13:29 -0400

    Added doctrings and usage.

--- build_and_deploy_container.sh
+++ build_and_deploy_container.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
+# Builds a container and deploys it.
+#
+# This has a bunch of steps:
+#   1 Create a Dockerfile,
+#   2 tag it appropriately,
+#   3 upload it to the Google Container Repository,
+#   4 fill in the deployment templates in the deployment/ directory,
+#   5 apply the deployment/ directory to the cluster.
+
 set -e
 set -x
 
-IMAGE_TAG=${1:?Please provide a tag for the image}
-GCR_REPO=${2:?Please provide a gcr.io url to upload to}
-CLOUD_PROJECT=${3:?Please specify the cloud project deploy to}
-CLUSTER_NAME=${4:?Please provide a cluster name to deploy to}
-CLUSTER_ZONE=${5:?Please provide a cluster zone to deploy to}
+USAGE="$0 <image tag> <gcr.io url> <cloud project> <custer name> <cluster zone> [<KEY> <VALUE>]*"
+IMAGE_TAG=${1:?Please provide a tag for the image: $USAGE}
+GCR_REPO=${2:?Please provide a gcr.io url to upload to: $USAGE}
+CLOUD_PROJECT=${3:?Please specify the cloud project deploy to: $USAGE}
+CLUSTER_NAME=${4:?Please provide a cluster name to deploy to: $USAGE}
+CLUSTER_ZONE=${5:?Please provide a cluster zone to deploy to: $USAGE}
 shift 5
 
 # All subsequent arguments must be name-value pairs, which means that there
 # should be an even number of arguments.
 [[ $[ $# % 2 ] == 0 ]] || exit 1
 
+# Build the image and tag it
+docker build . -t ${GCR_REPO}:${IMAGE_TAG}
+
+# Upload the image
+gcloud --project=${CLOUD_PROJECT} docker -- push ${GCR_REPO}:${IMAGE_TAG}
+
+# Fill in the deployment template(s)
 # Make a substitution in every file in the deployment/ directory
 function deployment_substitution() {
   KEY=$1
@@ -34,7 +51,6 @@ do
   deployment_substitution "{{${NAME}}}" "${VALUE}"
 done
 
-docker build . -t ${GCR_REPO}:${IMAGE_TAG}
-gcloud --project=${CLOUD_PROJECT} docker -- push ${GCR_REPO}:${IMAGE_TAG}
+# Apply the deployment/ configs to the cluster in the region
 gcloud --project=${CLOUD_PROJECT} container clusters get-credentials ${CLUSTER_NAME} --zone=${CLUSTER_ZONE}
 kubectl apply -f deployment/
