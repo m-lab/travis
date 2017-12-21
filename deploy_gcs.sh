6c37f6fa81fff243d9b8bd35888ec0cd14bc8b1c
Stephen Soltesz <soltesz@google.com>
2017-12-21 16:22:47 -0500

    Update to use key names from environment

--- deploy_gcs.sh
+++ deploy_gcs.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 #
-# deploy_gcs supports deploying build artifacts to GCS buckets using base64
-# encoded service account keys, such as those stored in travis-ci environments.
+# deploy_gcs supports deploying build artifacts to GCS buckets using service
+# account keys from the environment, such as those stored in travis-ci.
 #
 # Examples:
 #   # Copies all files matching pattern to the named folder, preserving names.
-#   deploy_gcs.sh "$SERVICE_ACCOUNT_mlab_sandbox" \
+#   deploy_gcs.sh SERVICE_ACCOUNT_mlab_sandbox \
 #       build/*.rpm \
 #       gs://example-mlab-sandbox/files/
 #
 #   # Copies specific file to a folder, preserving the name.
-#   deploy_gcs.sh "$SERVICE_ACCOUNT_mlab_sandbox" \
+#   deploy_gcs.sh SERVICE_ACCOUNT_mlab_sandbox \
 #       build/foobar.rpm \
 #       gs://example-mlab-sandbox/files/
 #
 #   # Copies specific file and renames it in GCS.
-#   deploy_gcs.sh "$SERVICE_ACCOUNT_mlab_sandbox" \
+#   deploy_gcs.sh SERVICE_ACCOUNT_mlab_sandbox \
 #       build/foobar.rpm \
 #       gs://example-mlab-sandbox/files/foobar-newname.rpm
 set -e
 set -u
 
-USAGE="Usage: $0 <key> <src> <dest>"
-KEY=${1:?Please provide the base64 encoded service account key: $USAGE}
+USAGE="Usage: $0 <keyname> <src> <dest>"
+KEYNAME=${1:?Please provide the service account keyname: $USAGE}
 SRC=${2:?Please provide a source file, dir, or pattern: $USAGE}
 DEST=${3:?Please provide a destination bucket with optional path: $USAGE}
 
@@ -31,7 +31,7 @@ source "${HOME}/google-cloud-sdk/path.bash.inc"
 source $( dirname "${BASH_SOURCE[0]}" )/gcloudlib.sh
 
 # Authenticate all operations using the given service account.
-activate_service_account "${KEY}"
+activate_service_account "${KEYNAME}"
 
 # Copy recursively to GCS with caching disabled.
 copy_nocache ${SRC} ${DEST}
