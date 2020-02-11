66c058dc20867362eccd3f1bede38fe96f93f136
yachang <yachang@users.noreply.github.com>
2020-02-11 13:56:16 -0500

    Update run_with_application_credentials.sh

--- run_with_application_credentials.sh
+++ run_with_application_credentials.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Run a cmd using service account credentials.
+# Run a cmd using service account credentials as application credentials.
 
 set -x
 set -e
@@ -8,14 +8,17 @@ set -e
 PROJECT=${1:?Please provide the GCP project id}
 KEYNAME=${2:?Please provide the service account keyname}
 BASEDIR=${3:?Please provide the path to the command to run}
-CMD=${4:?Please provide the binary name to run}
+shift 3
 
 local keyfile=$( mktemp )
 set +x; echo "${!KEYNAME}" > ${keyfile}
 export GOOGLE_APPLICATION_CREDENTIALS=${keyfile}
 
+set -x
+
 pushd "${BASEDIR}"
-  GCLOUD_PROJECT="${PROJECT}" "${CMD}"
+  # Run command given on the rest of the command line.
+  GCLOUD_PROJECT="${PROJECT}" $@
 popd
 
 exit 0
